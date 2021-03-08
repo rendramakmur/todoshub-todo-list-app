dbd3a6549620b8e83f4366f22a015e6e412630a5
rendramakmur <rendra1795@gmail.com>
2021-03-08 16:50:45 +0700

    Update config for deployment to Heroku

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const express = require('express')
 const errHandler = require('./middlewares/errHandler')
 const router = require('./routes')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const cors = require('cors')
 
 app.use(cors())
